0743aae3f8f25194ea2184ad8ab2e78baa18fc8f
Joe <jpfeiffer@bluemodus.com>
2015-07-22 23:10:32 -0600

    More physics

--- app/modules/game.js
+++ app/modules/game.js
@@ -3,5 +3,31 @@ module.exports = (function () {
 
 	Physics(function(world){
 		console.log('it appears to be working. (physics.js)');
+		world.on('step', function() {
+			world.render();
+			console.log('stepping');
+		});
+		
+		world.add( Physics.body('circle', {
+	        // x: renderer.width * 0.4
+	        // ,y: renderer.height * 0.3
+	        vx: 0.3
+	        ,radius: 80
+	        ,styles: {
+	            fillStyle: '#cb4b16'
+	            ,angleIndicator: '#72240d'
+	        }
+	    }));
+		
+		world.add([
+			Physics.behavior('constant-acceleration'),
+			Physics.behavior('body-impulse-response'),
+			Physics.behavior('body-collision-detection'),
+			Physics.behavior('sweep-prune')
+		]);
+		Physics.util.ticker.on(function( time ) {
+			console.log('ticking');
+	        world.step( time );
+	    });
 	});
 })();
