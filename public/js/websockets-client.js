6396b75d0e5f9a556b300c5a8a22d51a02439c39
Joe <jpfeiffer@bluemodus.com>
2015-07-26 00:35:34 -0600

    Switching to pixi

--- public/js/websockets-client.js
+++ public/js/websockets-client.js
@@ -4,31 +4,47 @@ var socket = eio('ws://localhost:2999');
 
 // var view = new fabric.StaticCanvas('viewport');
 
+var stage, renderer, test;
+
+window.onload = function(e) {
+  stage = new PIXI.Stage(0x66FF99);
+  
+  renderer = PIXI.autoDetectRenderer(400, 400);
+    
+  document.body.appendChild(renderer.view);
+  
+  test = new PIXI.Rectangle(0, 0, 20, 20);
+  
+  stage.addChild(test);
+  
+  requestAnimFrame( animate );
+}
+
+function animate() {
+ 
+    requestAnimFrame( animate );
+
+    // // just for fun, lets rotate mr rabbit a little
+    // bunny.rotation += 0.1;
+
+    // render the stage   
+    renderer.render(stage);
+}
 socket.on('open', function(){
   console.log('Opening');
   socket.on('message', function(data){
     // console.log(data);
     
     //Render info:
-    var one = new fabric.Rect({
-        width: 10, height: 10,
-        left: 0, top: 0,
-        fill: 'yellow'
-        // angle: 30
-    });
-    
-    view.add(one);
-  
     var bodies = data;
+    var body = bodies[0];
     
-    for (var i = 0; i < bodies.length; i++) {
-      var body = bodies[i];
-      one.top = body.y;
-      one.left = body.x;
-      // view.renderAll();
+    
+    // console.dir(test);
+    if (test) {
+      test.x = body.x;
+      test.y = body.y;
     }
-    
-    
     
   });
   socket.on('close', function(){
