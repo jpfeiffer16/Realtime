ed97947d71c79928f88a69ee8e8f13ea1f828457
Joe <jpfeiffer@bluemodus.com>
2015-07-26 22:42:25 -0600

    Made framrate independent of physics steps

--- app/modules/game.js
+++ app/modules/game.js
@@ -12,6 +12,85 @@ module.exports = (function () {
 				
 				// console.log(world.getBodies());
 				//construct the reduced-body array:
+				
+			});
+			
+			// world.add( Physics.body('circle', {
+		    //     x: 0
+		    //     ,y: 0
+		    //     ,vx: 4
+		    //     ,radius: 3
+			// 	,mass: 1
+			// 	,restitution: .5
+		    //     ,styles: {
+		    //         fillStyle: '#cb4b16'
+		    //         ,angleIndicator: '#72240d'
+		    //     }
+		    // }));
+		    //     x: 0
+		    //     ,y: 0
+		    //     ,vx: 4
+		    //     ,radius: 3
+			// 	,mass: 1
+			// 	,restitution: .5
+		    //     ,styles: {
+		    //         fillStyle: '#cb4b16'
+		    //         ,angleIndicator: '#72240d'
+		    //     }
+		    // }));
+			// world.add( Physics.body('rectangle', {
+		    //     x: 60
+		    //     ,y: 0
+		    //     ,vx: 0.3
+		    //     ,width: 10
+			// 	,height: 50
+			// 	,mass: 1
+			// 	,restitution: .5
+		    //     ,styles: {
+		    //         fillStyle: '#cb4b16'
+		    //     }
+		    // }));
+			
+			for (var i = 0; i < 200; i = i + 10) {
+				for (var j = 0; j < 200; j = j + 10) {
+					world.add( Physics.body('circle', {
+				        x: j
+				        ,y: i
+				        ,vx: 1
+				        ,radius: 3
+						,mass: 1
+						,restitution: .5
+				        ,styles: {
+				            fillStyle: '#cb4b16'
+				            ,angleIndicator: '#72240d'
+				        }
+				    }));
+				}
+			}
+			
+			var edgeBounce = Physics.behavior('edge-collision-detection', {
+	            aabb: viewportBounds
+	            ,restitution: .5
+	            ,cof: 0.8
+	        });
+	
+	        world.add( edgeBounce );
+			
+			world.add([
+				Physics.behavior('constant-acceleration'),
+				Physics.behavior('body-impulse-response'),
+				Physics.behavior('body-collision-detection'),
+				Physics.behavior('sweep-prune')
+			]);
+			// Physics.util.ticker.on(function( time ) {
+			// 	console.log('ticking');
+		    //     world.step( time );
+		    // });
+			
+			setInterval(function() {
+				world.step();
+			}, 10);
+			setInterval(function() {
 				var bodies = world.getBodies(),
 					reducedBodies = [];
 				
@@ -43,55 +122,7 @@ module.exports = (function () {
 				
 				//do send logic:
 				socket.send(JSON.stringify(reducedBodies));
-			});
-			
-			world.add( Physics.body('circle', {
-		        x: 0
-		        ,y: 0
-		        ,vx: 4
-		        ,radius: 3
-				,mass: 1
-				,restitution: .5
-		        ,styles: {
-		            fillStyle: '#cb4b16'
-		            ,angleIndicator: '#72240d'
-		        }
-		    }));
-			world.add( Physics.body('rectangle', {
-		        x: 60
-		        ,y: 0
-		        ,vx: 0.3
-		        ,width: 10
-				,height: 50
-				,mass: 1
-				,restitution: .5
-		        ,styles: {
-		            fillStyle: '#cb4b16'
-		        }
-		    }));
-			
-			var edgeBounce = Physics.behavior('edge-collision-detection', {
-	            aabb: viewportBounds
-	            ,restitution: .5
-	            ,cof: 0.8
-	        });
-	
-	        world.add( edgeBounce );
-			
-			world.add([
-				Physics.behavior('constant-acceleration'),
-				Physics.behavior('body-impulse-response'),
-				// Physics.behavior('body-collision-detection'),
-				Physics.behavior('sweep-prune')
-			]);
-			// Physics.util.ticker.on(function( time ) {
-			// 	console.log('ticking');
-		    //     world.step( time );
-		    // });
-			
-			setInterval(function() {
-				world.step();
-			}, 60);
+			}, 100);
 			
 			// world.unpause();
 		});
