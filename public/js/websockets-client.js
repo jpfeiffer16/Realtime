ba4304744ac7ce099de765ed10e2059dbf6a00af
Joe <jpfeiffer@bluemodus.com>
2015-07-28 22:32:19 -0600

    Client side rendering

--- public/js/websockets-client.js
+++ public/js/websockets-client.js
@@ -1,3 +1,4 @@
+/* global Physics */
 /* global PIXI */
 /* global fabric */
 /* global eio */
@@ -35,25 +36,85 @@ var socket = eio('ws://localhost:2999');
 
 socket.on('open', function(){
   console.log('Opening');
-  socket.on('message', function(data){
-    renderSet.clear();
+  Physics(function(world) {
     
-    //Render info:
-    var bodies = JSON.parse(data);
-    for (var i = 0; i < bodies.length; i++) {
-      var body = bodies[i];
-      // renderSet.lineStyle(2, 0xFF00FF, 1);
-      // renderSet.beginFill(0xFF00BB, 0.25);
-      if (body.objType == 'rect') {
-        // renderSet.drawRect((body.x) - (body.x / 2), (body.y) - (body.y / 2), body.width, body.height);
-      } else {
-        // renderSet.drawCircle(body.x, body.y, body.radius);
+    var renderer = Physics.renderer('pixi', {
+      el: 'viewport'
+    });
+    
+    world.add(renderer);
+    
+    
+    
+    
+    
+    
+    Physics.util.ticker.on(function( time ) {
+        world.step( time );
+    });
+    
+    
+    
+    socket.on('message', function(data){
+      // renderSet.clear();
+      
+      //Render info:
+      var bodies = JSON.parse(data);
+      for (var i = 0; i < bodies.length; i++) {
+        var body = bodies[i];
+        // renderSet.lineStyle(2, 0xFF00FF, 1);
+        // renderSet.beginFill(0xFF00BB, 0.25);
+        
+        // var geoType = body.objType == 'rect' ? 'recttangle'
+        if (body.objType == 'rect') {
+          //It's a rect
+          var exists = checkExists(body.uid);
+          if (!exists) {
+            //insert the body
+            world.add(Physics.body('rectangle', {
+              x: body.x,
+              y: body.y,
+              width: body.width,
+              height: body.height
+            }));
+          } else {
+            //Update the body
+            exists.x = body.x;
+            exists.y = body.y;
+          }
+        } else {
+          var exists = checkExists(body.uid);
+          if (!exists) {
+            //insert the body
+            world.add(Physics.body('circle', {
+              x: body.x,
+              y: body.y,
+              radiius: body.radius
+            }));
+          } else {
+            //Update the body
+            exists.x = body.x;
+            exists.y = body.y;
+          }
+        }
+
       }
-      // renderSet.endFill();
-    }
-    
-  });
-  socket.on('close', function(){
-    console.log('closing');
+      
+      function checkExists(uid) {
+        var query = Physics.query({
+          uid: uid
+        });
+        var exists = world.find(query);
+        if (exists != false) {
+          return exists;
+        } else {
+          return false;
+        }
+      }
+      
+    });
+    socket.on('close', function(){
+      console.log('closing');
+    });
   });
 });
